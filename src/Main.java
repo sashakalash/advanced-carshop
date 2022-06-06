8e38cfee0e03842952d4b4949bac58f471085689
Aleksandr Koloshtivin <Aleksandr.Koloshtivin@noveogroup.com>
2022-06-06 10:42:48 +0200

    Merge conflict has been fixed

--- src/Main.java
+++ src/Main.java
@@ -6,38 +6,18 @@ public class Main {
         carStore = new CarStore();
         carShop = new CarShop(carStore);
 
-//        Thread customer1 = new Thread(null, new Customer(carShop, "Покупатель 1"));
-//        Thread customer2 = new Thread(null, new Customer(carShop, "Покупатель 2"));
-//        Thread customer3 = new Thread(null, new Customer(carShop, "Покупатель 3"));
-//        customer1.start();
-//        customer2.start();
-//        customer3.start();
-<<<<<<< HEAD
-        new Thread(null, carShop::sellCar, "Покупатель 1").start();
+        Thread customer1 = new Thread(null, new Customer(carShop, "Покупатель 1"));
+        Thread customer2 = new Thread(null, new Customer(carShop, "Покупатель 2"));
+        Thread customer3 = new Thread(null, new Customer(carShop, "Покупатель 3"));
+        customer1.start();
+        customer2.start();
+        customer3.start();
+        while (carShop.soldCars != 2) {
+            Thread.onSpinWait();
+        }
 
-        new Thread(null, carShop::sellCar, "Покупатель 2").start();
-
-        new Thread(null, carShop::sellCar, "Покупатель 3").start();
-
-        new Thread(null, carShop::receiveCar, "Производитель Toyota").start();
-
-//        while (carShop.soldCars != 2) {
-//        Thread.onSpinWait();
-=======
-        new Thread(null, carShop::sellCar,  "Покупатель 1").start();
-
-        new Thread(null, carShop::sellCar,  "Покупатель 2").start();
-
-        new Thread(null, carShop::sellCar,  "Покупатель 3").start();
-
-        new Thread(null, carShop::receiveCar,  "Производитель Toyota").start();
-
-//        while (carShop.soldCars != 2) {
-//            continue;
->>>>>>> 0b9e40c07edf6bf36a5941ffc75315dc2d7618c9
-//        }
-//        customer1.interrupt();
-//        customer2.interrupt();
-//        customer3.interrupt();
+        customer1.interrupt();
+        customer2.interrupt();
+        customer3.interrupt();
     }
 }
